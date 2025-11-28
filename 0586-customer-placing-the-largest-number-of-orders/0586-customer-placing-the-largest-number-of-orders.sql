ce7f6fcf4145c3d206ac29c0e8bd8490e3e4e139
Pranab Debnath <62995713+pranabdnath@users.noreply.github.com>
2025-11-29 01:31:20 +0530

    Time: 3765 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,32 @@
+-- Window function
+/*SELECT customer_number
+FROM (
+    SELECT customer_number,
+           ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rn
+    FROM Orders
+    GROUP BY customer_number
+) x
+WHERE rn = 1;
+*/
+
+/*Using Subquery
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(*) = (
+    SELECT MAX(order_count)
+    FROM (
+        SELECT COUNT(*) AS order_count
+        FROM Orders
+        GROUP BY customer_number
+    ) x
+);
+*/
+
+/*EasyMethod*/
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
