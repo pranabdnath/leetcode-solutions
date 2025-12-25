f1d7d4ff98bb9f084d3b891d4832d65ff0e87f0e
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 16:06:05 +0530

    Time: 366 ms (48.46%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,13 @@
-select e.name as Employee
-from employee e
-join employee m
-on e.managerId = m.id
+/* Basically we need to find out the salary of an employee who's salary is greated than the manager*/
+/* So for this we need to map the employee id and amanger id so that sql can understand who is the manager here.*/
+
+
+
+select e.name as employee
+from employee e /* this is the employee table*/
+join employee m /* this is the manager table*/
+    on e.managerId=m.id /* what we are doing here is that we are mapping the both id's to find the manager*/
+/* so far we have done, now lets face the objective
+ to fine the salary of an emp> manager*/
+
 where e.salary>m.salary;
