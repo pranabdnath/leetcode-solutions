2bf3b3a1641467c59c8a271ce0ef36c98abbe0b1
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 17:13:02 +0530

    Time: 273 ms (71.21%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,5 +1,12 @@
-# Write your MySQL query statement below
-SELECT name, population, area
-FROM World
-WHERE area >=3000000
-OR population >=25000000;
+# find the big countries
+
+#cond: 1. they should be havi area of >= 3000000 km2
+# or population >= 25000000
+
+#lets solve this:
+
+select name, population, area
+from world
+         where population >= 25000000
+         or area >= 3000000
+order by name;
