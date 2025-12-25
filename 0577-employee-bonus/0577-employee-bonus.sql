f1ed1310471359bb8899f0634eec208ed82dd93d
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2025-12-25 16:53:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,9 @@
-SELECT E.name, B.bonus
-FROM Employee E
-LEFT JOIN Bonus B ON E.empId=B.empId
-WHERE B.bonus < 1000 OR B.bonus IS NULL;
+# find the employee who has a bonus less than 1000.
+# and also the employee who haven't received any
+
+select e.name, e.bonus
+from employee e
+left join bonus
+        on e.empId = b.empId
+where b.bonus < 1000
+or b.bonus is null;
